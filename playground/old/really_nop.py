f1282ccd0b0bb6f31dce50ea6e1e8330e108b24c
Maurice Makaay <mmakaay1@xs4all.net>
2020-01-31 17:14:32 +0100

    Removed old build files.

--- playground/old/really_nop.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from nabaztag.bytecode import Response, BytecodeFrame
-
-message = Response().build()
-
-with open("really_nop.bin", "wb") as f:
-    f.write(bytes(message))
